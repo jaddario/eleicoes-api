dedc9d468db80adbe4e59f69ad5f310ac45353ac
José Addario <j.addario@gmail.com>
2023-05-28 20:18:02 +0100

    Initial commit

---
+++ src/main/java/com/addario/eleicoesapi/EleicoesApiApplication.java
@@ -0,0 +1,13 @@
+package com.addario.eleicoesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EleicoesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EleicoesApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/addario/eleicoesapi/EleicoesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.addario.eleicoesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EleicoesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
